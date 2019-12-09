7ed8dbdc9e74e231479f4822205c65e5dabcf7b9
Carlos <carloslago@opendeusto.es>
2019-12-10 00:30:06 +0100

    Start demo

--- qGrover/qGrover/urls.py
+++ qGrover/qGrover/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import qGroverApp.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio, name="Inicio")
 ]
--- qGrover/qGroverApp/views.py
+++ qGrover/qGroverApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+import numpy as np
+import math
+
+# importing Qiskit
+from qiskit import IBMQ, BasicAer, Aer
+from qiskit.providers.ibmq import least_busy
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, execute
+
+# import basic plot tools
+from qiskit.visualization import plot_histogram
+
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'inicio.html')
